7fa1558a8ca6261d385a3596b39cae01d7664227
NotRin7 <102989232+NotRin7@users.noreply.github.com>
2026-02-06 01:23:35 +0000

    Update Arch build script with final linker fixes

--- quick-build-arch.sh
+++ quick-build-arch.sh
@@ -2,8 +2,9 @@
 # High-compatibility build script for Arch Linux
 set -e
 export BDB_PREFIX="$(pwd)/db4"
-rm -rf db4 db-4.8.30.NC
-./contrib/install_db4.sh . --with-mutex=POSIX/pthreads CPPFLAGS="-P -D_GNU_SOURCE" LDFLAGS="-lpthread"
+if [ ! -d "$BDB_PREFIX" ]; then
+    ./contrib/install_db4.sh . --with-mutex=POSIX/pthreads CPPFLAGS="-P -D_GNU_SOURCE" LDFLAGS="-lpthread"
+fi
 ./autogen.sh
-./configure BDB_LIBS="-L${BDB_PREFIX}/lib -ldb_cxx-4.8" BDB_CFLAGS="-I${BDB_PREFIX}/include" --with-boost=/usr --with-boost-libdir=/usr/lib --with-gui=qt5 --disable-tests --disable-bench --disable-reduce-exports LDFLAGS="-lpthread -lrt" LIBS="-lboost_system -lboost_filesystem -lboost_thread -lboost_chrono"
+./configure BDB_LIBS="-L${BDB_PREFIX}/lib -ldb_cxx-4.8" BDB_CFLAGS="-I${BDB_PREFIX}/include" --with-boost=/usr --with-boost-libdir=/usr/lib --with-gui=qt5 --disable-tests --disable-bench --disable-reduce-exports LDFLAGS="-L/usr/lib -lpthread -lrt" CPPFLAGS="-I/usr/include" LIBS="-lpthread -lrt"
 make -j$(nproc)
